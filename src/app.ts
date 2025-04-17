b491ffec73707d7008be7b53465394c0d5dafde5
kenithlabonite21@gmail.com <kenithlabonite>
2025-04-17 21:46:16 +0800

    Recommitted.

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from "express"
 import * as dotevnv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
-
+import { userRouter } from "./users/users.routes"
+import { productRouter } from "./products/product.routes"
 
 dotevnv.config()
 
@@ -19,7 +20,10 @@ app.use(express.urlencoded({extended: true}))
 app.use(cors())
 app.use(helmet())
 
+app.use('/', userRouter)
+app.use('/', productRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 })
+
